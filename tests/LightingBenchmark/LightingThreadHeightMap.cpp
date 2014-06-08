c49e2bf419fbe876d48940d5df8fc356279910a4
worktycho <work.tycho@gmail.com>
2014-06-08 12:24:58 +0100

    Update LightingThreadHeightMap.cpp

--- tests/LightingBenchmark/LightingThreadHeightMap.cpp
+++ tests/LightingBenchmark/LightingThreadHeightMap.cpp
@@ -19,6 +19,8 @@ typedef std::string AString;
 #define ASSERT(x)
 #define UNUSED(x)
 #define abstract
+#define override
+
 typedef long long Int64;
 typedef int       Int32;
 typedef short     Int16;
